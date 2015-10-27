eec5565ac1e7869eb5353831a756e73151bdcf6d
Guillaume Salmon <guisalmon@gmail.com>
2015-10-27 23:52:49 +0100

    One script to rule them all and in darkness bind them

--- magnetic/Load_details.sh
+++ magnetic/Load_details.sh
@@ -8,9 +8,11 @@ fi
 
 dir="img/photos/$1"
 titleFile="$dir/title"
-file="$1.html"
+file="./$1.html"
 photos="$dir/*"
 template="Detail_photos.html"
+
+file=${file##*/}
 
 title=$(head -n 1 $titleFile)
 
@@ -19,12 +21,13 @@ cp $template $file
 htmlTemplate="<div class=\"work\">\n\t\t\t<a href=\"inner.html\">\n\t\t\t\t<img src=\"_thumb_\" class=\"media\" alt=\"\"/>\n\t\t\t\t<div class=\"caption\">\n\t\t\t\t\t<div class=\"work_title\">\n\t\t\t\t\t\t<h1>_title_</h1>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t</a>\n\t\t</div>\n"
 
 portPhotos="$dir/Port*"
+thumb="$dir/thumb.jpg"
 
 for f in $portPhotos
 do
-    echo "Processing $f..."
     if [ ${f: -4} == ".png" ] || [ ${f: -4} == ".jpg" ]
     then
+        echo "Processing $f..."
         element="${htmlTemplate/_thumb_/$f}"
         element="${element/_title_/""}"
         sed -i "61i$element" $file
@@ -33,11 +36,11 @@ done
 
 for f in $photos
 do
-    echo "Processing $f..."
     if [ ${f: -4} == ".png" ] || [ ${f: -4} == ".jpg" ] 
     then
-        if [[ "$f" != $portPhotos ]]
+        if [[ "$f" != $portPhotos ]] && [ "$f" != $thumb ]
         then
+            echo "Processing $f..."
             element="${htmlTemplate/_thumb_/$f}"
             element="${element/_title_/""}"
             sed -i "61i$element" $file
