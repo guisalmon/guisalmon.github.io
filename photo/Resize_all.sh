0d412e38b709bc761c863ba331e2d98940f2625a
Guillaume Salmon <guisalmon@gmail.com>
2015-10-31 19:12:02 +0100

    Social links edited

--- photo/Resize_all.sh
+++ photo/Resize_all.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-files="img/photos/*/*.jpg"
-ports="img/photos/*/Port*.jpg"
+if [ $# -eq 0 ]
+then
+    files="img/photos/*/*.jpg"
+    ports="img/photos/*/Port*.jpg"
+else
+    files="img/photos/$1/*.jpg"
+    ports="img/photos/$1/Port*.jpg"
+fi
+
 
 for f in $files
 do
-    if [[ "$f" != $ports ]]
+    if [[ "$f" != $ports ]] && [[ "$f" != "thumb.jpg" ]]
     then
         echo $f
         mogrify -resize 1920x1280! $f
